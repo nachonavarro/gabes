a42ebcee3f02bf56c8237edc48618fb4d762060d
Nacho <ignacio.navarro14@imperial.ac.uk>
2018-05-25 10:31:11 +0200

    OT protocol for evaluator

--- gabes/ot.py
+++ gabes/ot.py
@@ -1,3 +1,7 @@
+from random import randint
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+
 def garbler_ot(m0, m1):
 	private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
 	d = private_key.private_numbers().d
@@ -10,3 +14,13 @@ def garbler_ot(m0, m1):
 	v = receive_data_from_evaluator()
 	k0, k1 = [(v - x) ** d % n for x in (x0, x1)]
 	send_data_to_evaluator(m0 + k0, m1 + k1)
+
+def evaluator_ot(b):
+	x0, x1, n, e = receive_data_from_garbler()
+	k = randint(2, n)
+	chosen_x = x1 if b else x0
+	v = (chosen_x + k ** e) % n
+	send_data_to_garbler(v)
+	t0, t1 = receive_data_from_garbler()
+	chosen_t = t1 if b else t0
+	m = chosen_t - k
