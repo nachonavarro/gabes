dc56cb7ab415a6f2e3fdec50c5eae6b8423d6fa0
Nacho <ignacio.navarro14@imperial.ac.uk>
2018-05-25 10:29:27 +0200

    Improve Circuit object by adding helper functions (get leaves, etc)

--- gabes/circuit.py
+++ gabes/circuit.py
@@ -1,49 +1,60 @@
 import re
-from anytree import Node, RenderTree
+from anytree import Node, RenderTree, PostOrderIter
 from itertools import takewhile, dropwhile
+from gate import Gate
 
-def to_circuit(file):
-	t = build_tree(file)
+class Circuit(object):
+	"""The :class:`Circuit` object holds all the gates and wires
+	composing the circuit. Internally, it is represented as a binary
+	tree.
+	"""
 
-def build_tree(file):
-	with open(file, 'r') as f:
-		expression = f.read().strip()
-		root = _build_tree(None, expression)
-	return root
+	def __init__(self, file):
+		self.tree = self.build_tree(file)
 
-def _build_tree(parent, expression):
-	left, gate, right = _separate(expression)
-	node = Node(gate, parent=parent) if parent else Node(gate)
+	def build_tree(self, file):
+		with open(file, 'r') as f:
+			expression = f.read().strip()
+			root = self._build_tree(None, expression)
+		return root
 
-	if len(left.split()) == 1:
-		Node(left, parent=node)
-	else:
-		_build_tree(node, left)
-	if len(right.split()) == 1:
-		Node(right, parent=node)
-	else:
-		_build_tree(node, right)
+	def _build_tree(self, parent, expression):
+		left, gate_type, right = self._separate(expression)
+		gate = Gate(gate_type)
+		node = Node(gate, parent=parent) if parent else Node(gate)
 
-	if not parent:
-		return node
+		if len(left.split()) == 1:
+			Node(left, parent=node)
+		else:
+			self._build_tree(node, left)
+		if len(right.split()) == 1:
+			Node(right, parent=node)
+		else:
+			self._build_tree(node, right)
 
-def _separate(expression):
-	if '(' not in expression:
-		return expression.split()
-	balance = 0
-	for i, char in enumerate(expression):
-		if char == '(':
-			balance += 1
-		elif char == ')':
-			balance -= 1
-		if balance == 0:
-			break
-	left, rest  = expression[1:i], expression[i + 1: -1]
-	gate, right = rest.split('(')
-	return left, gate.strip(), right
+		if not parent:
+			return node
 
-def draw_circuit(tree):
-	for pre, fill, node in RenderTree(tree):
-		print("%s%s" % (pre, node.name))
+	def _separate(self, expression):
+		if '(' not in expression:
+			return expression.split()
+		balance = 0
+		for i, char in enumerate(expression):
+			if char == '(':
+				balance += 1
+			elif char == ')':
+				balance -= 1
+			if balance == 0:
+				break
+		left, rest  = expression[1:i], expression[i + 1: -1]
+		gate, right = rest.split('(')
+		return left, gate.strip(), right
+
+	def draw_circuit(self):
+		for pre, fill, node in RenderTree(self.tree):
+			print("%s%s" % (pre, node.name))
+
+	def get_input_labels(self):
+		return [node.name for node in PostOrderIter(self.tree) if node.is_leaf]
 
 
