483b9b10739e1360b35c4706835d25f9035821af
Nacho <ignacio.navarro14@imperial.ac.uk>
2018-05-29 11:30:44 +0200

    Add the circuit.py that includes the tree and a method reconstruct it

--- gabes/circuit.py
+++ gabes/circuit.py
@@ -1,7 +1,10 @@
-import re
-from anytree import Node, RenderTree, PostOrderIter
+import copy
+
+from anytree import Node, RenderTree, PostOrderIter, LevelOrderGroupIter, PreOrderIter
 from itertools import takewhile, dropwhile
 from gate import Gate
+from wire import Wire
+
 
 class Circuit(object):
 	"""The :class:`Circuit` object holds all the gates and wires
@@ -11,6 +14,7 @@ class Circuit(object):
 
 	def __init__(self, file):
 		self.tree = self.build_tree(file)
+		self.input_wires = None
 
 	def build_tree(self, file):
 		with open(file, 'r') as f:
@@ -20,20 +24,36 @@ class Circuit(object):
 
 	def _build_tree(self, parent, expression):
 		left, gate_type, right = self._separate(expression)
-		gate = Gate(gate_type)
-		node = Node(gate, parent=parent) if parent else Node(gate)
+		node = self._build_gate(parent, left, gate_type, right)
+		gate = node.name
 
 		if len(left.split()) == 1:
-			Node(left, parent=node)
+			gate.left_wire.identifier = left
 		else:
 			self._build_tree(node, left)
 		if len(right.split()) == 1:
-			Node(right, parent=node)
+			gate.right_wire.identifier = right
 		else:
 			self._build_tree(node, right)
 
+		gate.garble()
+
 		if not parent:
 			return node
+
+	def _build_gate(self, parent, left, gate_type, right):
+		""" Ensure the parent's left wire is the left children's output wire
+		and the parent's right wire is the right children's output wire."""
+		is_left_leaf = len(left.split()) == 1
+		is_right_leaf = len(right.split()) == 1
+		gate = Gate(gate_type, create_left_wire=is_left_leaf, create_right_wire=is_right_leaf)
+		node = Node(gate, parent=parent) if parent else Node(gate)
+		if parent:
+			if parent.children[0] == node:
+				parent.name.left_wire = gate.output_wire
+			elif parent.children[1] == node:
+				parent.name.right_wire = gate.output_wire
+		return node
 
 	def _separate(self, expression):
 		if '(' not in expression:
@@ -50,11 +70,38 @@ class Circuit(object):
 		gate, right = rest.split('(')
 		return left, gate.strip(), right
 
+	def reconstruct(self, labels):
+		levels = [[node for node in children] for children in LevelOrderGroupIter(self.tree)][::-1]
+		for level in levels:
+			for node in level:
+				gate = node.name
+				if node.is_leaf:
+					garblers_label, evaluators_label = labels.pop(0), labels.pop(0)
+				else:
+					left_gate  = node.children[0].name
+					right_gate = node.children[1].name
+					garblers_label, evaluators_label = left_gate.chosen_label, right_gate.chosen_label
+				output_label = gate.ungarble(garblers_label, evaluators_label)
+				gate.chosen_label = output_label
+		return self.tree.name.chosen_label
+
+	def clean(self):
+		""" Cleans the circuit from any private labels in preparation for Bob. """
+		circ = copy.deepcopy(self)
+		for node in PreOrderIter(circ.tree):
+			gate = node.name
+			gate.left_wire = gate.right_wire = gate.output_wire = None
+		return circ
+
 	def draw_circuit(self):
 		for pre, fill, node in RenderTree(self.tree):
-			print("%s%s" % (pre, node.name))
+			print("{}{}".format(pre, node.name))
 
-	def get_input_labels(self):
-		return [node.name for node in PostOrderIter(self.tree) if node.is_leaf]
+	def get_input_wires(self):
+		if not self.input_wires:
+			leaves = [node.name for node in PostOrderIter(self.tree) if node.is_leaf]
+			input_wires = [wire for leaf in leaves for wire in [leaf.left_wire, leaf.right_wire]]
+			self.input_wires = input_wires
+		return self.input_wires
 
 
