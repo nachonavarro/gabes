74997f9f2dec6df99bb9f1c2971a07f6cda923cd
Nacho <ignacio.navarro14@imperial.ac.uk>
2018-05-29 11:29:38 +0200

    Include the label object with improvements

--- gabes/label.py
+++ gabes/label.py
@@ -3,20 +3,27 @@ import base64
 
 
 class Label(object):
-    """The :class:`Label` object, which contains the label
-    that will represent either the boolean *False* or *True* for a particular gate.
-    """
-    def __init__(self, represents, gate=None, size=32):
-        self.gate  = gate
-        self.label = self.generate_label(size)
-        self.represents = represents
+	"""The :class:`Label` object, which contains the label
+	that will represent either the boolean *False* or *True* for a particular gate.
+	"""
 
-    def __repr__(self):
-        return str(self.label)
+	def __init__(self, represents, pp_bit=None):
+		self.label = os.urandom(32)
+		self.represents = represents
+		self.pp_bit = pp_bit
 
-    def __str__(self):
-        return str(self.label)
+	def __repr__(self):
+		return str(self.to_base64())
 
-    @staticmethod
-    def generate_label(size):
-        return base64.urlsafe_b64encode(os.urandom(size))
+	def __str__(self):
+		return str(self.to_base64())
+
+	def __int__(self):
+		return int.from_bytes(self.label, byteorder='big')
+
+	def to_base64(self):
+		return base64.urlsafe_b64encode(self.label)
+
+	@staticmethod
+	def int_to_bytes(n):
+		return int.to_bytes(n, length=32, byteorder='big')
