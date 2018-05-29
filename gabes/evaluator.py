204560e0d9f50d308d7dbae7054551793b472274
Nacho <ignacio.navarro14@imperial.ac.uk>
2018-05-29 11:27:38 +0200

    Add the two main functions (garbler and evaluator)

--- gabes/evaluator.py
+++ gabes/evaluator.py
@@ -1,17 +1,43 @@
-import socket
-import pickle
-from utils import ask_for_inputs
+import network as net
+import time
 
-def evaluator(address, circuit_file):
-	sock   = connect_evaluator(address)
-	inputs = ask_for_inputs(circuit_file)
-	serialized = pickle.dumps(inputs)
-	sock.send(serialized)
-	data = sock.recv(1024)
+from ot import evaluator_ot
+from utils import ask_for_inputs
+from circuit import Circuit
+
+def evaluator(args):
+	sock   = net.connect_evaluator(args.address)
+	idents = request_wire_identifiers(sock)
+	inputs = ask_for_inputs(idents)
+	labels = request_labels(sock, idents, inputs)
+	circ   = request_cleaned_circuit(sock)
+	secret_output = circ.reconstruct(labels)
+	final_output  = learn_output(sock, secret_output)
+	print("The final output of the circuit is: {}".format(final_output))
 	sock.close()
 
-def connect_evaluator(address):
-	ip, port = address.split(':')
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect((ip, int(port)))
-	return sock
+def request_cleaned_circuit(sock):
+	circuit = net.receive_data(sock)[0]
+	return circuit
+
+def request_wire_identifiers(sock):
+	identifiers = net.receive_data(sock)
+	net.send_ack(sock)
+	return identifiers
+
+def request_labels(sock, identifiers, evaluator_inputs):
+	labels = []
+	for identifier in identifiers:
+		if identifier in evaluator_inputs:
+			chosen_bit = evaluator_inputs[identifier]
+			secret_label = evaluator_ot(sock, chosen_bit)
+		else:
+			secret_label = net.receive_data(sock)[0]
+			net.send_ack(sock)
+		labels.append(secret_label)
+	return labels
+
+def learn_output(sock, secret_output):
+	net.send_data(sock, secret_output)
+	final_output = net.receive_data(sock)[0]
+	return final_output
--- gabes/garbler.py
+++ gabes/garbler.py
@@ -1,18 +1,50 @@
-import socket
-import pickle
+import network as net
+import time
+import copy
 
-def garbler(address, circuit_file):
-	sock = connect_garbler(address)
-	client, addr = sock.accept()
-	data = client.recv(1024)
-	hel = pickle.loads(data)
-	print(hel)
+from circuit import Circuit
+from utils import ask_for_inputs
+from ot import garbler_ot
+
+def garbler(args):
+	print("Welcome, garbler. Waiting for the evaluator...")
+	sock, client = net.connect_garbler(args.address)
+	circ = Circuit(args.circuit)
+	identifiers = hand_over_wire_identifiers(client, circ)
+	inputs = ask_for_inputs(identifiers)
+	hand_over_labels(client, circ, inputs)
+	hand_over_cleaned_circuit(client, circ)
+	final_output = learn_output(client, circ)
+	print("The final output of the circuit is: {}".format(final_output))
 	sock.close()
 
-def connect_garbler(address):
-	ip, port = address.split(':')
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	sock.bind((ip, int(port)))
-	sock.listen(1)
-	return sock
+def hand_over_wire_identifiers(client, circ):
+	identifiers = [wire.identifier for wire in circ.get_input_wires()]
+	net.send_data(client, *identifiers)
+	net.wait_for_ack(client)
+	return identifiers
+
+def hand_over_cleaned_circuit(client, circ):
+	new_circ = circ.clean()
+	net.send_data(client, new_circ)
+
+def hand_over_labels(client, circ, garbler_inputs):
+	for wire in circ.get_input_wires():
+		if wire.identifier in garbler_inputs:
+			chosen_bit = garbler_inputs[wire.identifier]
+			secret_label = wire.true_label if chosen_bit == '1' else wire.false_label
+			net.send_data(client, secret_label)
+			net.wait_for_ack(client)
+		else:
+			false_label = copy.deepcopy(wire.false_label)
+			true_label  = copy.deepcopy(wire.true_label)
+			# Clean before sending so that the evaluator does not learn anything.
+			false_label.represents = true_label.represents = None
+			garbler_ot(client, false_label, true_label)
+
+def learn_output(client, circ):
+	output_label = net.receive_data(client)[0]
+	output_gate  = circ.tree.name
+	output = output_label.to_base64() == output_gate.output_wire.true_label.to_base64()
+	net.send_data(client, output)
+	return output
