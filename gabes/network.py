75cba9a9ee2a082f97d64c68fab870435fb803fa
Nacho <ignacio.navarro14@imperial.ac.uk>
2018-05-30 19:03:37 +0200

    Handle packets that may be larger than the prefixed size of the buffer on network.py

--- gabes/network.py
+++ gabes/network.py
@@ -1,46 +1,55 @@
 import socket
 import pickle
 import time
+import struct
 
 def connect_garbler(address):
-	ip, port = address.split(':')
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	sock.bind((ip, int(port)))
-	sock.listen(1)
-	client, addr = sock.accept()
-	return sock, client
+    ip, port = address.split(':')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((ip, int(port)))
+    sock.listen(1)
+    client, addr = sock.accept()
+    return sock, client
 
 def connect_evaluator(address):
-	ip, port = address.split(':')
-	print("Welcome, evaluator. Waiting for the garbler...", flush=True)
-	while True:
-		try:
-			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			sock.connect((ip, int(port)))
-			break
-		except Exception as e:
-			time.sleep(1)
+    ip, port = address.split(':')
+    print("Welcome, evaluator. Waiting for the garbler...", flush=True)
+    while True:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((ip, int(port)))
+            break
+        except Exception as e:
+            time.sleep(1)
+    return sock
 
-	return sock
+def send_data(sock, data):
+    data = pickle.dumps(data)
+    size = struct.pack('>I', len(data))
+    sock.send(size + data)
 
-def send_data(sock, *data, serialize=True):
-	if serialize:
-		data = pickle.dumps(data)
-	sock.send(data)
+def receive_data(from_whom):
+    size = _receive_data(from_whom, 4)
+    size = struct.unpack('>I', size)[0] 
+    data = _receive_data(from_whom, size)
+    pickled_data = pickle.loads(data)
+    return pickled_data
 
-def receive_data(from_whom, buff=8192, deserialize=True):
-	if deserialize:
-		data = pickle.loads(from_whom.recv(buff))
-	else:
-		data = from_whom.recv(buff)
-	return data
+def _receive_data(sock, num_bytes):
+    data = b''
+    while len(data) < num_bytes:
+        packet = sock.recv(num_bytes - len(data))
+        if not packet:
+            break
+        data += packet
+    return data
 
 def send_ack(sock):
-	send_data(sock, 'ACK')
+    send_data(sock, 'ACK')
 
 def wait_for_ack(sock):
-	while True:
-		msg = receive_data(sock)[0]
-		if msg == 'ACK':
-			break
+    while True:
+        msg = receive_data(sock)
+        if msg == 'ACK':
+            break
