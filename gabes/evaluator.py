b4a3176f6f6c73674043a5eacfbd6476a95b14cb
Nacho <ignacio.navarro14@imperial.ac.uk>
2018-05-30 19:02:10 +0200

    Remove *args when receiving from the socket

--- gabes/evaluator.py
+++ gabes/evaluator.py
@@ -17,7 +17,7 @@ def evaluator(args):
 	sock.close()
 
 def request_cleaned_circuit(sock):
-	circuit = net.receive_data(sock)[0]
+	circuit = net.receive_data(sock)
 	return circuit
 
 def request_wire_identifiers(sock):
@@ -32,12 +32,12 @@ def request_labels(sock, identifiers, evaluator_inputs):
 			chosen_bit = evaluator_inputs[identifier]
 			secret_label = evaluator_ot(sock, chosen_bit)
 		else:
-			secret_label = net.receive_data(sock)[0]
+			secret_label = net.receive_data(sock)
 			net.send_ack(sock)
 		labels.append(secret_label)
 	return labels
 
 def learn_output(sock, secret_output):
 	net.send_data(sock, secret_output)
-	final_output = net.receive_data(sock)[0]
+	final_output = net.receive_data(sock)
 	return final_output
